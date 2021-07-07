0ee809aaf4121014797c29cc0a936c6488c0711a
Agonictise <agonictise@gmail.com>
2021-07-07 17:28:15 +0900

    Custom control, DelegateProxy 추가

--- main/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
+++ main/MasteringRxSwift/CustomExtension/CustomBinderViewController.swift
@@ -25,52 +25,78 @@ import RxSwift
 import RxCocoa
 
 class CustomBinderViewController: UIViewController {
-   
-   let bag = DisposeBag()
-   
-   @IBOutlet weak var valueLabel: UILabel!
-   
-   @IBOutlet weak var colorPicker: UISegmentedControl!
-   
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      colorPicker.rx.selectedSegmentIndex
-         .map { index -> UIColor in
-            switch index {
-            case 0:
-               return UIColor.red
-            case 1:
-               return UIColor.green
-            case 2:
-               return UIColor.blue
-            default:
-               return UIColor.black
-            }
-         }
-         .subscribe(onNext: { [weak self] color in
-            self?.valueLabel.textColor = color
-         })
-         .disposed(by: bag)
-
-      colorPicker.rx.selectedSegmentIndex
-         .map { index -> String? in
-            switch index {
-            case 0:
-               return "Red"
-            case 1:
-               return "Green"
-            case 2:
-               return "Blue"
-            default:
-               return "Unknown"
-            }
-         }
-         .bind(to: valueLabel.rx.text)
-         .disposed(by: bag) 
-   }
+    
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var colorPicker: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //      colorPicker.rx.selectedSegmentIndex
+        //         .map { index -> UIColor in
+        //            switch index {
+        //            case 0:
+        //               return UIColor.red
+        //            case 1:
+        //               return UIColor.green
+        //            case 2:
+        //               return UIColor.blue
+        //            default:
+        //               return UIColor.black
+        //            }
+        //         }
+        //         .subscribe(onNext: { [weak self] color in
+        //            self?.valueLabel.textColor = color
+        //         })
+        //         .disposed(by: bag)
+        //
+        //      colorPicker.rx.selectedSegmentIndex
+        //         .map { index -> String? in
+        //            switch index {
+        //            case 0:
+        //               return "Red"
+        //            case 1:
+        //               return "Green"
+        //            case 2:
+        //               return "Blue"
+        //            default:
+        //               return "Unknown"
+        //            }
+        //         }
+        //         .bind(to: valueLabel.rx.text)
+        //         .disposed(by: bag)
+        /**
+         위 코드는 rx를 통해 UIColor 와 String 을 설정
+         아래는 binder 를 이용해 둘을 한꺼번에 설정
+         */
+        colorPicker.rx.selectedSegmentIndex
+            .bind(to: valueLabel.rx.segmentedValue)
+            .disposed(by: bag)
+    }
 }
 
-
+extension Reactive where Base: UILabel {
+    var segmentedValue: Binder<Int> {
+        return Binder(self.base) { label, index in
+            switch index {
+            case 0:
+                label.text = "Red"
+                label.textColor = .red
+            case 1:
+                label.text = "green"
+                label.textColor = .green
+            case 2:
+                label.text = "Blue"
+                label.textColor = .blue
+            default:
+                label.text = "Unknown"
+                label.textColor = .black
+            }
+        }
+    }
+}
 
--- main/MasteringRxSwift/CustomExtension/DelegateProxyViewController.swift
+++ main/MasteringRxSwift/CustomExtension/DelegateProxyViewController.swift
@@ -27,28 +27,69 @@ import RxCocoa
 import MapKit
 
 class DelegateProxyViewController: UIViewController {
-   
-   let bag = DisposeBag()
-   
-   @IBOutlet weak var mapView: MKMapView!
-   
-   let locationManager = CLLocationManager()
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      
-   }
+    
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        locationManager.rx.didUpdateLocations
+            .subscribe(onNext: { locations in
+                print(locations)
+            }).disposed(by: bag)
+        
+        locationManager.rx.didUpdateLocations
+            .map{ $0[0] }
+            .bind(to: mapView.rx.center)
+            .disposed(by: bag)
+    }
 }
 
 
 extension Reactive where Base: MKMapView {
-   public var center: Binder<CLLocation> {
-      return Binder(self.base) { mapView, location in
-         let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
-         self.base.setRegion(region, animated: true)
-      }
-   }
+    public var center: Binder<CLLocation> {
+        return Binder(self.base) { mapView, location in
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+            self.base.setRegion(region, animated: true)
+        }
+    }
 }
 
 
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    weak private(set) var locationManager: CLLocationManager?
+    
+    init(locationManager: CLLocationManager) {
+        self.locationManager = locationManager
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+    static func registerKnownImplementations() {
+        self.register {
+            RxCLLocationManagerDelegateProxy(locationManager: $0)
+        }
+    }
+}
+
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    var didUpdateLocations: Observable<[CLLocation]> {
+        return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+            .map { parameters in
+                return parameters[1] as! [CLLocation]
+            }
+    }
+}
